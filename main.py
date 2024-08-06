f70b26102406b6d0a66ab01737a16fe90c44e140
Elijah Karori Orito <elijakarori23@gmail.com>
2024-08-06 18:52:05 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,11 +2,23 @@ from typing import Optional
 from routers import users
 from routers import items
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 
 app = FastAPI()
 app.include_router(users.router)
 app.include_router(items.router)
 
+origins = [
+    "*",
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
